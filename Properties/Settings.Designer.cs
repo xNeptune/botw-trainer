babc4d56175697249d1c8c023bfdd097d126b0f8
Jonathan Monkton <jon.monkton@gmail.com>
2017-03-10 22:36:11 +0000

    v2.3.1

--- Properties/Settings.Designer.cs
+++ Properties/Settings.Designer.cs
@@ -49,7 +49,7 @@ namespace BotwTrainer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("2.3")]
+        [global::System.Configuration.DefaultSettingValueAttribute("2.3.1")]
         public string CurrentVersion {
             get {
                 return ((string)(this["CurrentVersion"]));
