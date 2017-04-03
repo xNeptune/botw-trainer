d6a5be4c9daae57638d9dcc99cad7ce6c1b102e7
xNeptune <Captainskittlez410@gmail.com>
2017-04-02 20:19:33 -0400

    Added a save coords function

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -641,6 +641,10 @@
                                 CoordsX.Content = string.Format("{0}", Math.Round(xFloat, 2));
                                 CoordsY.Content = string.Format("{0}", Math.Round(yFloat, 2));
                                 CoordsZ.Content = string.Format("{0}", Math.Round(zFloat, 2));
+                                CoordsAll.Text = string.Join(", ", "\"Name\":", 
+                                    "\"LocX\":", CoordsX.Content, 
+                                    "\"LocY\":", CoordsY.Content, 
+                                    "\"LocZ\":", CoordsZ.Content);
                                 run = this.connected && EnableCoords.IsChecked == true;
                             });
 
@@ -651,6 +655,25 @@
             {
                 Dispatcher.Invoke(() => this.LogError(ex, "Coords Tab"));
             }
+        }
+        
+        private void CoordsSaveClick(object sender, RoutedEventArgs e)
+        {
+            StreamWriter log;
+
+            if (!File.Exists("savedcoords.txt"))
+            {
+                log = new StreamWriter("savedcoords.txt");
+            }
+            else
+            {
+                log = File.AppendText("savedcoords.txt");
+            }
+            log.WriteLine(CoordsAll.Text);
+            log.WriteLine();
+
+            log.Close();
+
         }
 
         private void ConnectClick(object sender, RoutedEventArgs e)
